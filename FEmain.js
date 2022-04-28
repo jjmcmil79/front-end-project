cd2263410a08d650d0532b5f1a49271321890725
Joshua McMiller <jjmcmi113r@gmail.com>
2022-04-28 14:06:18 -0500

    Final product for assignment w_o updates

--- FEmain.js
+++ FEmain.js
@@ -1,5 +1,5 @@
 let body = document.getElementsByTagName('body')
-
+// document.body.style.backgroundImage = "images/mountains.jpg";
 //set all of my id variables
 const container = document.getElementById('container')
 const dateElement = document.getElementById('date');
@@ -17,6 +17,15 @@ const futureForecastElement = document.getElementById('weather-forecast-item');
     currentInfo.classList.add('current-info')
     container.appendChild(currentInfo)
 
+    const currentContainer = document.createElement('div')
+    currentContainer.classList.add('current-container')
+    currentInfo.appendChild(currentContainer)
+
+    // const mapContainer = document.createElement('div')
+    // mapContainer.classList.add('map-container')
+    // mapContainer.id = 'map-container'
+    // currentInfo.appendChild(mapContainer)
+
     const dateContainer = document.createElement('div')
     dateContainer.classList.add('date-container')
     currentInfo.appendChild(dateContainer)
@@ -27,10 +36,10 @@ const futureForecastElement = document.getElementById('weather-forecast-item');
     timeContainer.textContent = '12:30'
     dateContainer.appendChild(timeContainer)
 
-    const amPm = document.createElement('span')
-    amPm.id = 'am-pm'
-    amPm.textContent = 'PM'
-    timeContainer.appendChild(amPm)
+    // const amPm = document.createElement('span')
+    // amPm.id = 'am-pm'
+    // amPm.textContent = 'PM'
+    // dateContainer.appendChild(amPm)
     
     const date = document.createElement('div')
     date.classList.add('date')
@@ -38,39 +47,42 @@ const futureForecastElement = document.getElementById('weather-forecast-item');
     date.textContent = 'Thursday, 28 April'
     dateContainer.appendChild(date)
 
-    const todayForecast = document.createElement('div')
-    todayForecast.classList.add('today')
-    todayForecast.id = 'current-temp'
-    dateContainer.appendChild(todayForecast)
-    let todayImg = document.createElement('img')
-    todayImg.classList.add('w-icon')
-    todayImg.src = "http://openweathermap.org/img/wn/10d@2x.png"
-    todayImg.alt = "weather icon" 
-    todayImg.id = 'w-icon'
-    todayForecast.appendChild(todayImg)
+    const others = document.createElement('div')
+    others.classList.add('others')
+    others.id = 'current-weather-items'
+    others.style.visibility = 'hidden';
+    currentContainer.appendChild(others)
 
     const other = document.createElement('div')
     other.classList.add('other')
     other.id = 'other'
-    todayForecast.appendChild(other)
+    others.appendChild(other)
 
     const currentDay = document.createElement('div')
     currentDay.classList.add('current-day')
     currentDay.id ='current-day'
     other.appendChild(currentDay)
 
+    let todayImg = document.createElement('img')
+    todayImg.classList.add('w-icon')
+    // todayImg.src = "http://openweathermap.org/img/wn/10d@4x.png"
+    todayImg.alt = "weather icon" 
+    todayImg.id = 'w-icon'
+    
+    other.appendChild(todayImg)
+
+    const currentDesc = document.createElement('div')
+    currentDesc.classList.add('current-desc')
+    currentDesc.id ='current-desc'
+    other.appendChild(currentDesc)
+    
     const currentTemp = document.createElement('div')
     currentTemp.classList.add('current-temperature')
     currentTemp.id ='current-temperature'
     other.appendChild(currentTemp)
-    
-
-    const others = document.createElement('div')
-    others.classList.add('others')
-    others.id = 'current-weather-items'
-    dateContainer.appendChild(others)
 
     const placeContainer = document.getElementById('place-container')
+    placeContainer.style.visibility = 'hidden';
     const futureForecast = document.getElementById('future-forecast')
     
     const timeZone = document.createElement('div')
@@ -115,8 +127,10 @@ const API_Key = '705b7e2bf903340ad3d67654088d5536';
 function getGeo(){
     $.get('http://api.openweathermap.org/geo/1.0/direct?q=' + searchBox.value + ',US&limit=5&appid=' + API_Key, (data) => {
         console.log(data[0])
-        // searchBox.value = ''
+        searchBox.value = ''
         getWeather(data)
+        // getMap(data)
+        
         })
 
 }
@@ -128,6 +142,16 @@ function getWeather(data){
 
         })
 }
+// getMap()
+// function getMap(data){
+//     $.get('https://tile.openweathermap.org/map/temp_new/13/32/97.png?appid=' + API_Key, (map) => {
+//         console.log(map)
+//         // searchBox.value = ''
+//         mapContainer.src = map
+        
+//         })
+
+// }
 
 //create weather item containers
 function createWeatherItems(){
@@ -145,7 +169,7 @@ function createWeatherItems(){
         tempDiv1.appendChild(tempDiv2)
         tempDiv1.appendChild(tempDiv3)
         }
-        
+         
 }
 let humidity = document.querySelector('#item-name1')
 let pressure = document.getElementById('item-name2')
@@ -154,16 +178,23 @@ let sunrise = document.getElementById('item-name4')
 let sunset = document.getElementById('item-name5')
 let Temp = document.getElementById('current-temperature')
 let nameOfDay = document.getElementById('current-day')
+let description = document.getElementById('current-desc')
+let map =document.getElementById('map-container')
 humidity.textContent = 'Humidity'
 pressure.textContent = 'Pressure'
 windSpeed.textContent = 'Wind Speed'
 sunrise.textContent = 'Sunrise'
 sunset.textContent = 'Sunset'
+nameOfDay.textContent = 'Today'
+Temp.innerHTML = '105°'
+description.innerHTML = 'Sunny'
+// map.innerHTML = 'https://tile.openweathermap.org/map/temp_new/13/32/97.png?appid=' + API_Key
+document. body.style.backgroundImage = "url('images/los-angeles.jpg')";
 
 
 
 
-createForecastItems(3)
+createForecastItems()
 
 
 function createForecastItems(days){
@@ -180,18 +211,24 @@ function createForecastItems(days){
         tempImg.src = "http://openweathermap.org/img/wn/10d@2x.png"
         tempImg.alt = "weather icon" 
         tempImg.id = 'w-icon' + [i]
+        let tempDiv5 = document.createElement('div')
+        tempDiv5.classList.add('fore-desc')
+        tempDiv5.id ='fore-desc' + [i]
         let tempDiv3 = document.createElement('div')
         tempDiv3.classList.add('temp')
         tempDiv3.id ='hi-temp' + [i]
         let tempDiv4 = document.createElement('div')
         tempDiv4.classList.add('temp')
         tempDiv4.id ='low-temp' + [i]
+        
              
         weatherForecast.appendChild(tempDiv1)
         tempDiv1.appendChild(tempDiv2)
         tempDiv1.appendChild(tempImg)
+        tempDiv1.appendChild(tempDiv5)
         tempDiv1.appendChild(tempDiv3)
         tempDiv1.appendChild(tempDiv4)
+        
         
         }
         
@@ -201,7 +238,7 @@ function showWeatherData (wData){
     // let {humidity, pressure, sunrise, sunset, wind_speed} = wData.current;
 // console.log(wData)
     timeZone.innerHTML = wData.timezone;
-    country.innerHTML = wData.lat + 'N ' + wData.lon+'E'
+    country.innerHTML = wData.lat + '°N ' + wData.lon+'°E'
     
     let vhumidity = document.getElementById('item-value1')
     let vpressure = document.getElementById('item-value2')
@@ -211,15 +248,23 @@ function showWeatherData (wData){
     let vTemp = document.getElementById('current-temperature')
     let vnameOfDay = document.getElementById('current-day')
     let vImg = document.getElementById('w-icon')
+    // let vMovie = document.getElementById('myVideo')
+    let vdescription = document.getElementById('current-desc')
     vhumidity.innerHTML = wData.current.humidity
-    vpressure.textContent = wData.current.pressure
-    vwindSpeed.textContent = wData.current.wind_speed
-    vTemp.innerHTML = wData.current.temp
-    vImg.src = 'http://openweathermap.org/img/wn//'+ wData.daily[0].weather[0].icon + '@2x.png'
-    vnameOfDay.textContent = window.moment(wData.daily[0].dt * 1000).format('dddd')
+    vpressure.textContent = wData.current.pressure + ' Hg'
+    vwindSpeed.textContent = wData.current.wind_speed + ' mph'
+    vTemp.innerHTML = Math.round(wData.current.temp) + '°'
+    vImg.src = 'http://openweathermap.org/img/wn//'+ wData.daily[0].weather[0].icon + '@4x.png'
+    vnameOfDay.innerHTML = window.moment(wData.daily[0].dt * 1000).format('dddd')
     vsunrise.textContent = window.moment(wData.current.sunrise * 1000).format('H:mm a')
     vsunset.textContent = window.moment(wData.current.sunset * 1000).format('H:mm a')
-      console.log(wData.current.temp)
+    // vMovie.src = "images/Rain.mp4"; 
+    vdescription.innerHTML = wData.current.weather[0].description.toUpperCase() 
+    others.style.visibility = 'visible';
+    placeContainer.style.visibility = 'visible';
+    let bkImage = 'images/' + wData.current.weather[0].main + '.jpg'
+    document. body.style.backgroundImage = 'url('+ bkImage +')';
+    console.log(wData, vdescription)
     
     
     
@@ -231,35 +276,15 @@ function showWeatherData (wData){
             let dayName = document.getElementById('day' +[i])
             let tempHi = document.getElementById('hi-temp' +[i])
             let tempLow = document.getElementById('low-temp' +[i])
-            otherDayImg.src = 'http://openweathermap.org/img/wn//'+ wData.daily[i].weather[0].icon + '@4x.png'
-            // futureForecastElement.appendChild(otherDayImg)
-            tempHi.innerHTML = wData.daily[i].temp.max
-            tempLow.innerHTML = wData.daily[i].temp.min
+            let tempDesc = document.getElementById('fore-desc' +[i])
+            otherDayImg.src = 'http://openweathermap.org/img/wn//'+ wData.daily[i].weather[0].icon + '@2x.png'
+            
+            tempHi.innerHTML = 'High: ' + Math.round(wData.daily[i].temp.max) + '°'
+            tempLow.innerHTML = 'Low: ' + Math.round(wData.daily[i].temp.min) + '°'
             dayName.innerHTML = window.moment(wData.daily[i].dt * 1000).format('dddd')
-            // futureForecastElement.appendChild(tempHi)
+            tempDesc.innerHTML = wData.daily[i].weather[0].description.toUpperCase()
         }
-            //   currentTempElement.innerHTML = 
-            //   `<img src="http://openweathermap.org/img/wn//${icon}@4x.png" alt="weather icon" class="w-icon">`
-            //   <div class="other">
-            //       <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
-            //       <div class="temp">Night - ${day.temp.night}&#176;C</div>
-            //       <div class="temp">Day - ${day.temp.day}&#176;C</div>
-            //   </div>
-              
-              
-        //   }
-        //   else{
-        //     //   otherDayForecast += 
-        //     //   <div class="weather-forecast-item">
-        //     //       <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
-        //     //       <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-        //     //       <div class="temp">Night - ${day.temp.night}&#176;C</div>
-        //     //       <div class="temp">Day - ${day.temp.day}&#176;C</div>
-        //     //   </div>
-              
-        //     //   
-        //   }
-    //   console.log(tempHi)
+            
 }
     
 searchBtn.addEventListener('click', getGeo);
